ee96faa25da90dd9e085e1258daa35622d3d8a91
sanchousmak <sanchous.mak@gmail.com>
2026-07-14 21:11:23 +0300

    v0.2.0

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+const gallery = {
+
+    painting: [
+
+        {
+            title: "Memory of Summer",
+            image: "catalog/painting/1.jpg"
+        },
+
+        {
+            title: "Silence",
+            image: "catalog/painting/2.jpg"
+        }
+
+    ],
+
+    graphic: [
+
+        {
+            title: "Morning",
+            image: "catalog/graphic/1.jpg"
+        }
+
+    ],
+
+    illustration: [
+
+        {
+            title: "Flowers",
+            image: "catalog/illustration/1.jpg"
+        }
+
+    ]
+
+};
